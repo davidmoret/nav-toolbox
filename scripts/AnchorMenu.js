5b200b289724eea46c7df93efd53f61a02853c28
David <dmoret@insite.coop>
2021-03-09 12:46:21 +0100

    correctifs AnchorMenu

---
+++ scripts/AnchorMenu.js
@@ -0,0 +1,72 @@
+import { utils } from '@insite/utils';
+import { ScrollSpy } from "./ScrollSpy.js";
+
+export class AnchorMenu {
+    /**
+     * @constructor
+     * @param {HTMLElement} container
+     * @param {Object} options
+     */
+    constructor(container, options = {}) {
+        this.container = container;
+        this.menu = document.createElement('nav');
+        this.options = Object.assign({}, {
+            ratio: '.7',
+            dataSelector: 'data-heading'
+        }, options);
+
+        // On créé le html de la nav et on l'injecte dans le container
+        this.menu.setAttribute('class', 'anchorMenu');
+        this.container.prepend(this.menu);
+        this.buildNav(this.menu, this.container.querySelectorAll(`[${this.options.dataSelector}]`));
+        new ScrollSpy({
+            ratio: this.options.ratio
+        });
+
+    }
+
+    /**
+     * @method splitNavToPanels
+     * @param {HTMLElement} thatMenu
+     * @param {NodeList} allHeadings
+     */
+    buildNav(thatMenu, allHeadings) {
+
+        /**
+         * @param {HTMLElement} item
+         * @param {string} anchor
+         */
+        const setSectionHeight = function(item, el){
+            const distance = utils.getDistanceBetweenElements(item, utils.getNextSibling(item, `[${this.options.dataSelector}]`));
+            el.style.height = distance + 'px';
+        };
+
+
+        /**
+         * @param {HTMLElement} item
+         * @param {string} anchor
+         */
+        const buildSection = function(item, anchor){
+            let el = document.createElement('div');
+            el.classList.add('anchor-section');
+            el.setAttribute('id', anchor);
+            item.appendChild(el);
+            if (utils.getNextSibling(item, `[${this.options.dataSelector}]`) !== undefined){
+                // On reset la hauteur de la section fantôme si resize
+                setSectionHeight(item, el);
+                window.addEventListener('resize', utils.debounce(function(){
+                    setSectionHeight(item, el);
+                }, 500));
+            }
+        };
+
+        allHeadings.forEach(function(item) {
+            let txt = item.getAttribute(this.options.dataSelector);
+            let id = txt.replace(/\W/g,'_');
+            thatMenu.append(utils.createLinkWithClass('lvl-item' + item.tagName, '#' + id, txt));
+            buildSection(item, id);
+        });
+
+    }
+
+}
---
+++ scripts/ScrollSpy.js
@@ -0,0 +1,58 @@
+import { utils } from "@insite/utils";
+
+export class ScrollSpy {
+
+
+    constructor(options = {}) {
+        this.observer = null;
+        this.options = Object.assign({}, {
+            selector: 'div.anchor-section'
+        }, options);
+        this.spies = document.querySelectorAll(this.options.selector);
+
+
+        let observe = function (elems, that) {
+            function callback(entries, observer) {
+                entries.forEach(function(entry){
+                    if (entry.intersectionRatio > 0) {
+                        that.activate(entry.target);
+                    }
+                });
+            }
+            // Si observer dejà présent on le delete
+            if(that.observer !== null) {
+                elems.forEach(elem => that.observer.unobserve(elem));
+            }
+            // Permet d'observer un trait d'1px à 30% du haut de l'écran
+            const y = Math.round(window.innerHeight * that.options.ratio);
+            that.observer = new IntersectionObserver(callback, {
+                rootMargin: `-${window.innerHeight - y - 1 }px 0px -${y}px 0px`
+            });
+            that.spies.forEach(elem => that.observer.observe(elem));
+        };
+
+        // Si il y a des élements à observer
+        if(this.spies.length > 0) {
+            let that = this;
+            observe(this.spies, this);
+            window.addEventListener('resize', utils.debounce(function(){
+                observe(that.spies, that);
+            }, 500));
+        }
+
+    }
+
+
+    activate(elem) {
+        const id = elem.getAttribute('id');
+        const anchor = document.querySelector(`a[href="#${id}"]`);
+        if(anchor === null){
+            return null;
+        }
+        anchor.parentElement
+            .querySelectorAll('.active')
+            .forEach(node => node.classList.remove('active'));
+        anchor.classList.add('active');
+    }
+
+}
